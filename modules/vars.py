916cbc71d59819b1a9c687545f3d3d54397acd77
rahulkhatri00 <116899776+rahulkhatri00@users.noreply.github.com>
2024-05-29 11:18:20 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "2511244"
-api_hash = "eec08d89fa5a627345966a73390231ad"
-bot_token = "6531753932:AAGMZyipxX11OG0VQDCT4lZ2mIiKcVtlMH0"
+api_id = "27161113"
+api_hash = "65c4b51f54049ebe22bc01fd9a95575b"
+bot_token = "6806206800:AAHpkIk7pL3RyvJ4EOPS5Vbk2m79u03S6Ss"
