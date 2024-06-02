61257a63e4335413eaaaa3d65ccb016be5186da2
rahulkhatri00 <116899776+rahulkhatri00@users.noreply.github.com>
2024-06-02 23:21:38 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "27355149"
 api_hash = "68bcb1ab22bd80bbae9d66ccddf540c2"
-bot_token = "7350135417:AAEjnqZhUX-IeARKYIz9NF9CK_DkMTJkv0c"
+bot_token = "7378952429:AAGJcprLeKeKw9b9oIdPsUy8rZKzh71SJ6w"
