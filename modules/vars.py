143124817b25c7253fe3538c91d6c36d7f1d50d5
johnbhaigithub <149451117+johnbhaigithub@users.noreply.github.com>
2024-08-25 19:32:58 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "27355149"
-api_hash = "68bcb1ab22bd80bbae9d66ccddf540c2"
-bot_token = "6754086528:AAHSuT5wsbUdrM0FbIvqUISpq6hPHc61Iqw"
+api_id = "22118129"
+api_hash = "43c66e3314921552d9330a4b05b18800"
+bot_token = "7291944908:AAGZDI6ukT6pEGyhE7XKeABeWvbAJvnflns"
